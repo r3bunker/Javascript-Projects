5017aa31d2525f9293d257c664613500955b5c31
r3bunker <r3bunker@gmail.com>
2020-08-27 10:19:27 -0600

    add basic js 2

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function mySecondFunction() {
+    var date = "What a great day";
+    var result = date.fontcolor("red");
+    document.getElementById("demo").innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
