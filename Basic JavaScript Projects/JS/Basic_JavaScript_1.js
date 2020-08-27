09e970c3136cbaa933e495f739e232b50f1b4f23
r3bunker <r3bunker@gmail.com>
2020-08-27 09:48:08 -0600

    Add Basic JS Projects
    window.alert()
    document.write()
    function()
    text coloring

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+var A = "Sally said: \"This is a string\"";
+var B = "- Micahel Scott";
+window.alert(A + B);
+
+document.write(B);
+
+
+function myFirstFunction() { //Defininng my function
+    var str = "This text is green"; //This is the string being displayed
+    var result = str.fontcolor("green"); //Changes test to green
+    document.getElementById("Green_Text").innerHTML = result; //assigns the ID
+}
