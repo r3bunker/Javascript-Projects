af1409680073c07bfa24090af799c2ccd8cbcfe2
r3bunker <r3bunker@gmail.com>
2020-09-01 10:38:38 -0600

    Add JS comments to assignment 9

--- Basic JavaScript Projects/JS/Basic_JavaScript_9.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_9.js
@@ -1,26 +1,26 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex); //sets display slide to 1
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n); //adds slides one after another
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideIndex = n); //sets the current slide to slideIndex, making the slideIndex = currentSlides
 }
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
+    if (n > slides.length) { slideIndex = 1 } //resets slideIndex to 1
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none"; //hides the other slides
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", ""); //displays 3 dots
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].style.display = "block"; //displays one image/slide at a time
+    dots[slideIndex - 1].className += " active"; //dots at the bottom are always active
 }
--- Basic JavaScript Projects/JS/Basic_JavaScript_9_countdown.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_9_countdown.js
@@ -1,18 +1,18 @@
 function countdown() {
-    var seconds = document.getElementById("seconds").value;
+    var seconds = document.getElementById("seconds").value; //displays seconds
 
     function tick() {
-        seconds = seconds - 1;
+        seconds = seconds - 1; //counts down by 1
         timer.innerHTML = seconds;
         if (seconds > 0) {
 
-            setTimeout(tick, 1000);
+            setTimeout(tick, 1000); //stops the timer at 0 seconds
         }
         if (seconds == 0) {
-            alert("Time's Up!");
+            alert("Time's Up!"); //alerts the window "Time's up! when it reaches 0"
 
         }
 
     }
-    tick();
+    tick(); //displays the count
 }
